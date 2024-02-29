5a5a2dafc091cc03262866b1f6425b5a96aefbca
Thang Nguyen <nguyenthang.fe@gmail.com>
2024-03-01 06:51:19 +0700

    Remove payment module

--- backend/src/domain/interfaces/IFPayment.ts
+++
@@ -1,9 +0,0 @@
-import { Document, Types } from 'mongoose';
-
-export interface IFPaymentModel extends Document {
-  orderId: string
-  amount: number
-  provider?: string
-  status?: string
-  orders: Types.ObjectId[]
-}
--- backend/src/modules/v1/payment/payment.services.ts
+++
@@ -1,1 +0,0 @@
-export const paymentServices = async (): Promise<boolean> => true;
